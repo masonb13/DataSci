6c4b460adcdfbbf82d5b307aa349bebdb98e9554
mfb425 <mfb425@lehigh.edu>
2022-10-03 13:27:16 -0400

    Outputs CSV

---
+++ Models/clust.sh
@@ -0,0 +1,26 @@
+!/bin/bash
+
+SBATCH --partition=health,lts,hawkcpu,infolab,engi,eng
+
+# Request 1 hour of computing time
+SBATCH --time=5:00:00
+SBATCH --ntasks=1
+
+# Give a name to your job to aid in monitoring
+SBATCH --job-name covidForecastinvaccinecombine
+
+# Write Standard Output and Error
+SBATCH --output="myjobvaccinecombinedata.%j.%N.out"
+cd ${SLURM_SUBMIT_DIR} # cd to directory where you submitted the job
+
+
+
+
+
+export PYTHONPATH=$PYTHONPATH:$HOME/pythonpkgs
+
+python3 test.py 
+
+exit
+
+sbatch clust.bat
