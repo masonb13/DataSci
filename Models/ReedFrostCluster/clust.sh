caac0963ab413e035a77f6e8c3975b02f03ceb4f
mfb425 <mfb425@lehigh.edu>
2022-10-17 14:24:52 -0400

    Prepare to test sbatch script

--- Models/ReedFrostCluster/clust.sh
+++ Models/ReedFrostCluster/clust.sh
@@ -3,7 +3,7 @@
 SBATCH --partition=health,lts,hawkcpu,infolab,engi,eng
 
 # Request 1 hour of computing time
-SBATCH --time=1:00:00
+SBATCH --time=0:02:00
 SBATCH --ntasks=3 --nodes=1
 
 # Give a name to your job to aid in monitoring
@@ -12,10 +12,6 @@ SBATCH --job-name reedFrostNetworkModel
 # Write Standard Output and Error
 SBATCH --output="reedFrost.%j.%N.out"
 cd ${SLURM_SUBMIT_DIR} # cd to directory where you submitted the job
-
-
-
-
 
 export PYTHONPATH=$PYTHONPATH:$HOME/pythonPackages
 
