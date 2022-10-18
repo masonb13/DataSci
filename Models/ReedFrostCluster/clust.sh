45b59d645829ef98e1c020163bb8ff6f68dfc133
mfb425 <mfb425@lehigh.edu>
2022-10-18 10:10:53 -0400

    added #s to clust

--- Models/ReedFrostCluster/clust.sh
+++ Models/ReedFrostCluster/clust.sh
@@ -1,16 +1,16 @@
-!/bin/bash
+#!/bin/bash
 
-SBATCH --partition=health,lts,hawkcpu,infolab,engi,eng
+#SBATCH --partition=health,lts,hawkcpu,infolab,engi,eng
 
 # Request 1 hour of computing time
-SBATCH --time=0:02:00
-SBATCH --ntasks=3 --nodes=1
+#SBATCH --time=0:02:00
+#SBATCH --ntasks=3 --nodes=1
 
 # Give a name to your job to aid in monitoring
-SBATCH --job-name reedFrostNetworkModel
+#SBATCH --job-name reedFrostNetworkModel
 
 # Write Standard Output and Error
-SBATCH --output="reedFrost.%j.%N.out"
+#SBATCH --output="reedFrost.%j.%N.out"
 cd ${SLURM_SUBMIT_DIR} # cd to directory where you submitted the job
 
 export PYTHONPATH=$PYTHONPATH:$HOME/pythonPackages
