955052c8142a9a3c97442d4deb7c499d4645c445
Mason Filip Bursac <mfb425@sol.cc.lehigh.edu>
2022-11-04 15:32:47 -0400

    ran the batch script

--- Models/ReedFrostCluster/metaSlurm.sh
+++ Models/ReedFrostCluster/metaSlurm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=health,lts,hawkcpu,infolab,engi,eng
 #--Request 1 hour of computing time
-#SBATCH --time=72:30:00
+#SBATCH --time=48:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #--Give a name to your job to aid in monitoring
